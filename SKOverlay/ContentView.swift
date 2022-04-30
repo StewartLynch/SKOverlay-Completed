3d20710b13db670b871c52ec48c99ef613b7cebc
Stewart Lynch <slynch@createchsol.com>
2022-04-30 13:25:17 -0700

    Starter Project

--- SKOverlay/ContentView.swift
+++ SKOverlay/ContentView.swift
@@ -11,8 +11,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Image("launchScreen")
+                Spacer()
+            }
+            .navigationTitle("SKOverlay")
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
